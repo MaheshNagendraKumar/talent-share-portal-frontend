f335be9376c336d0fa3cbf568451bfe9bd318911
Mahesh Nagendra Kumar Karri <maheshka@maveric-systems.com>
2023-12-28 20:07:14 +0530

    app.js modifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Register from './pages/registration/Register';
+import Dashboard from './pages/dashBoard/Dashboard';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route path='/' element={<><Home /></>} />
+            <Route path='/register' element={<><Register /></>} />
+            <Route path='/login' element={<><Login /></>} />
+            <Route path='/dashboard' element={<><Dashboard /></>} />
+          </Routes>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
